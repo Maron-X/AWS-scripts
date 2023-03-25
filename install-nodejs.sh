0f2e62ef6294bd61087485785595c03f26015263
Maron-X <120732521+Maron-X@users.noreply.github.com>
2023-03-25 17:19:37 +0200

    Create install-nodejs.sh

---
+++ install-nodejs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+cd ~
+
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+
+sudo bash nodesource_setup.sh
+
+sudo apt install nodejs -y
+
+sudo apt install build-essential -y
+
